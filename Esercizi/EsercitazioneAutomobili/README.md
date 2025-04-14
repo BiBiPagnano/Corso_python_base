b77e64e2ad0554251cb8b21191b3b7ded976923f
Jacopo Vitale <jacopo.vitale96@gmail.com>
2025-04-14 15:30:48 +0200

    refactor

--- Esercizi/EsercitazioneAutomobili/README.md
+++
@@ -1,3 +0,0 @@
-# Automobili App
-
-Tool per la gestione dei tuoi veicoli su ruote
--- Esercizi/EsercitazioneAutomobili/src/main.py
+++
@@ -1,7 +0,0 @@
-from moduli import Bicicletta, Automobile, SistemaUtils
-
-
-class App():
-
-    def __init__(self, ) -> None:
-        pass
